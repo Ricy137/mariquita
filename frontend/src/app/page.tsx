daa112a8f534900da42913ce32eae43899a27a62
Ricy <ricyzhang951@gmail.com>
2024-04-05 23:06:04 -0700

    feat: scroll animation

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,12 @@
 import RockImg from '@/assets/imgs/rock.jpg';
 import DeserImg from '@/assets/imgs/desert.jpg';
 import Image from 'next/image';
+import FloatItems from './FloatItems';
 
 export default function Home() {
   return (
     <main className="relative left-[200px] w-[calc(100%-200px)] min-h-screen">
-      <div className="relative grid grid-cols-[33%_67%]">
+      <div id="Mariquita" className="relative grid grid-cols-[33%_67%]">
         <Image
           src={RockImg}
           alt="Rock"
@@ -24,12 +25,7 @@ export default function Home() {
         draggable={false}
         className="pointer-events-none select-none"
       /> */}
-        <div className="p-[24px] absolute left-[33%] top-[50%] flex items-center justify-center w-[300px] h-[300px] text-[#ffffff] -translate-y-[40%] -translate-x-[100px] bg-[#000000] z-[1]">
-          introduction introduction introduction
-        </div>
-        <div className="absolute right-[33%] top-[50%] text-[36px] text-[#01ff00]">
-          Mariquita
-        </div>
+        <FloatItems />
       </div>
       <div className="h-[700px]" />
     </main>
