db59775cd09feb5db092e41d53584e0872f15219
Ricy <ricyzhang951@gmail.com>
2024-04-09 20:50:42 -0700

    feat: clear floating

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -19,13 +19,6 @@ export default function Home() {
           draggable={false}
           className="row-span-2 pointer-events-none select-none"
         />
-        {/* <Image
-        src={RockImg}
-        alt="Rock"
-        draggable={false}
-        className="pointer-events-none select-none"
-      /> */}
-        <FloatItems />
       </div>
       <div className="h-[700px]" />
     </main>
