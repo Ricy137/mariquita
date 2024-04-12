56b6caf0ec20b19cad9006947ee272218280c37e
Ricy <ricyzhang951@gmail.com>
2024-04-12 16:56:04 -0700

    feat: add bg images

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -11,35 +11,34 @@ export default function Home() {
       >
         <div className="p-[2rem] outline -outline-offset-[0.5px] outline-1 outline-[#ebebeb]">
           <p>
-            Since xxx, I targeted my relentless pursuit of the cutting edge of
+            Since xxx, I started my relentless pursuit of the cutting edge of
             blockchain technology...
           </p>
         </div>
-        <div className="col-span-2 outline -outline-offset-[0.5px] outline-1 outline-[#ebebeb]">
-          456
-        </div>
-        <div className="row-span-3 outline -outline-offset-[0.5px] outline-1 outline-[#ebebeb]">
-          789
-        </div>
+        <div className="col-span-2 bg-[url('/rock.jpg')] bg-fixed bg-cover bg-center rounded-[2rem] outline -outline-offset-[0.5px] outline-1 outline-[#ebebeb]" />
+        {/* <div className="col-span-2 outline -outline-offset-[0.5px] outline-1 outline-[#ebebeb]">
+          <div className="w-full h-full rounded-[2rem] outline -outline-offset-[0.5px] outline-1 outline-[#ebebeb]" />
+        </div> */}
+        <div className="row-span-3 bg-[url('/desert.jpg')] bg-fixed bg-cover bg-center rounded-[2rem] outline -outline-offset-[0.5px] outline-1 outline-[#ebebeb]" />
         <div className="p-[2rem] outline -outline-offset-[0.5px] outline-1 outline-[#ebebeb]">
-          <p>feature1</p>
+          <p>Professional (feature1)</p>
         </div>
-        <div></div>
-        <div></div>
+        <div className="bg-[url('/rock.jpg')] bg-fixed bg-cover bg-center rounded-[2rem]" />
+        <div className="bg-[url('/rock.jpg')] bg-fixed bg-cover bg-center rounded-[2rem]" />
         <div className="p-[2rem] outline -outline-offset-[0.5px] outline-1 outline-[#ebebeb]">
-          <p>feature2</p>
+          <p>Blockchain native (feature2) </p>
         </div>
         <div className="p-[2rem] outline -outline-offset-[0.5px] outline-1 outline-[#ebebeb]">
           <p>feature3</p>
         </div>
-        <div className="outline -outline-offset-[0.5px] outline-1 outline-[#ebebeb]"></div>
+        <div className="bg-[url('/rock.jpg')] bg-fixed bg-cover bg-center rounded-[2rem] outline -outline-offset-[0.5px] outline-1 outline-[#ebebeb]" />
         <div className="p-[2rem] outline -outline-offset-[0.5px] outline-1 outline-[#ebebeb]">
           <p>
             I've published over xxx pieces, cooperated with org xyz, abc... and
             invovled numerous topics like xxx
           </p>
         </div>
-
+        <div className="bg-[url('/rock.jpg')] bg-fixed bg-cover bg-center rounded-[2rem]" />
         {/* <Image
           src={RockImg}
           alt="Rock"
@@ -53,6 +52,7 @@ export default function Home() {
           className="row-span-2 pointer-events-none select-none"
         /> */}
       </div>
+      <div className="w-full h-[700px]" />
     </main>
   );
 }
