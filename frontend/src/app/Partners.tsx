1d6d02bee0ee00da8efa516768d091335ce890cc
Ricy <ricyzhang951@gmail.com>
2024-04-29 13:26:34 -0700

    feat: Carousel

--- frontend/src/app/Partners.tsx
+++ frontend/src/app/Partners.tsx
@@ -1,18 +1,15 @@
-import { PARTNERS } from '@/utils/constants';
+import { PARTNERS, Partner } from '@/utils/constants';
+import Image from 'next/image';
 
 const Partners: React.FC = () => {
   return (
-    <div className="w-full overflow-x-hidden text-[5.625rem] outline outline-1 outline-[#ebebeb]">
-      <div className="py-[1.75px] flex flex-row items-center gap-x-[6rem] animate-[scroll_10s_linear_infinite] w-[200%]">
+    <div className="w-full overflow-x-hidden outline outline-1 outline-[#ebebeb]">
+      <div className="grid grid-cols-6 animate-[scroll_20s_linear_infinite] w-[200%]">
         {PARTNERS.map((partner) => (
-          <div className="whitespace-nowrap" key={partner}>
-            {partner}
-          </div>
+          <PartnerItem key={partner.name} {...partner} />
         ))}
         {PARTNERS.map((partner) => (
-          <div className="whitespace-nowrap" key={`${partner}-2`}>
-            {partner}
-          </div>
+          <PartnerItem key={partner.name} {...partner} />
         ))}
       </div>
     </div>
@@ -20,3 +17,24 @@ const Partners: React.FC = () => {
 };
 
 export default Partners;
+
+const PartnerItem: React.FC<Partner> = ({
+  name,
+  title,
+  logo,
+  description,
+  link,
+}) => {
+  return (
+    <div className="py-[1.75rem] px-[1.5rem] w-full grid grid-cols-[64px_auto] grid-rows-[auto_auto] gap-[1rem] border-l border-l-1 border-[#ebebeb]">
+      <a href={link}>
+        <Image src={logo} alt={name} width={64} height={64} />
+      </a>
+      <div className="font-medium content-center">
+        <p>{name}</p>
+        <p>{title}</p>
+      </div>
+      <p className="col-start-2">{description}</p>
+    </div>
+  );
+};
--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,1 +1,34 @@
-export const PARTNERS = ['Surge', 'Cybrid', 'New Society'];
+export interface Partner {
+  name: string;
+  title: string;
+  logo: string;
+  description: string;
+  link: string;
+}
+
+export const PARTNERS: Partner[] = [
+  {
+    name: 'Dušanka Seratlić',
+    title: 'Content Lead with Surge',
+    link: 'https://www.surgewomen.io/',
+    logo: '/surge.png',
+    description:
+      'Mariquita does a great job communicating complex ideas in an accessible and friendly way. They excel in all aspects of content creation: from on-page SEO and detailed research to internal communication and accepting feedback with grace. ',
+  },
+  {
+    name: 'Lee Cocking',
+    title: 'Head of Product at Cybrid',
+    link: 'https://www.cybrid.xyz/',
+    logo: '/cybrid.png',
+    description:
+      'Mariquita is a talented writer, and a pleasure to collaborate with. Her content is deeply researched, and she has an impressive ability to weave in the perfect mix of brand elements, positioning and market intel, all while skillfully cross-referencing with previous works and calls-to-action. ',
+  },
+  {
+    name: 'Enrique Menendez',
+    title: 'Editor-in-Chief at New Society',
+    logo: '/ns.png',
+    link: 'https://nwscty.xyz/products/decentralization-please-save-culture-foundation-edition',
+    description:
+      'Mariquita is a talented writer, and a pleasure to collaborate with. Her content is deeply researched, and she has an impressive ability to weave in the perfect mix of brand elements, positioning and market intel, all while skillfully cross-referencing with previous works and calls-to-action. ',
+  },
+];
